9da4dd1f8ed9f4dc24e92ea9ec7dacc0e16be1a6
Wiebe-Jan <wiebejan_91@hotmail.com>
2026-09-21 15:39:15 +0200

    update filenames

--- wp-disable-trackback-and-spam.php
+++ wp-disable-trackback-and-spam.php
@@ -6,7 +6,7 @@ namespace ValkemediaDisableTrackbackSpam;
 Plugin Name: Valkemedia Disable trackback & spam
 Plugin URI: https://valkemedia.nl/
 Description: Disable comments, trackback & spam, remove wp_generator tag from wp theme
-Version: 1.0.4
+Version: 1.0.5
 Update URI: https://github.com/wimwam/wp-disable-trackback-and-spam
 Author: Wiebe-Jan Valkema
 Author URI: https://valkemedia.nl/
@@ -32,8 +32,8 @@ class valkemedia_disable_trackback_and_spam
         new GitHubUpdater(
             'wimwam/wp-disable-trackback-and-spam',
             plugin_basename(__FILE__),
-            '1.0.4',
-            'valkemedia-disable-trackback-and-spam'
+            '1.0.5',
+            'wp-disable-trackback-and-spam'
         );
 
         remove_action('wp_head', 'wp_generator');
